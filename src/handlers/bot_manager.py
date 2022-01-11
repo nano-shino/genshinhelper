35f50ad85e7307460cb6d2bb7107595ff9ba0572
Nano <nanorobot.cf@gmail.com>
2022-01-10 16:29:05 -0800

    Add update and restart command for bot

--- src/handlers/bot_manager.py
+++ src/handlers/bot_manager.py
@@ -1,8 +1,13 @@
+import asyncio
+import os
+import sys
+
 import discord
 from discord import SlashCommandGroup
 from discord.ext import commands
 
 from common import guild_level
+from common.logging import logger
 from interfaces.route_loader import load_images
 
 
@@ -21,6 +26,39 @@ class BotCommandHandler(commands.Cog):
         if not self.start_up:
             await load_images(self.bot)
             self.start_up = True
+
+    @bot.command(
+        description="Restarts the bot",
+        guild_ids=guild_level.get_guild_ids(level=5),
+    )
+    async def restart(self, ctx):
+        await ctx.respond("Restarting...")
+        args = [sys.executable] + sys.argv
+        logger.info(f"Restarting the bot: {args}")
+        os.execv(sys.executable, args)
+
+    @bot.command(
+        description="Updates the bot",
+        guild_ids=guild_level.get_guild_ids(level=5),
+    )
+    async def update(self, ctx):
+        logger.info("Updating the bot")
+
+        proc = await asyncio.create_subprocess_exec(
+            "git", "pull",
+            stdout=asyncio.subprocess.PIPE,
+            stderr=asyncio.subprocess.PIPE)
+
+        stdout, stderr = await proc.communicate()
+
+        logger.info(f'Command git pull exited with {proc.returncode}')
+
+        if stdout:
+            logger.info(f'[stdout]\n{stdout.decode()}')
+        if stderr:
+            logger.critical(f'[stderr]\n{stderr.decode()}')
+
+        await ctx.respond("Bot updated")
 
     @bot.command(
         description="Reloads route images",
