686aea3d48720e552a56ef0435ead727e60b9bd5
Nano <nanorobot.cf@gmail.com>
2022-03-31 19:14:55 -0700

    Fix resin/teapot cap notification, reduce API calls.

--- src/handlers/resin_cap.py
+++ src/handlers/resin_cap.py
@@ -17,8 +17,7 @@ from datamodels.scheduling import ScheduledItem, ItemType
 class ResinCapReminder(commands.Cog):
     RESIN_KEY = ItemType.RESIN_CAP  # Check if user enabled resin notification
     TEAPOT_KEY = ItemType.TEAPOT_CAP  # Check if user enabled teapot notification
-    CHECK_INTERVAL = 60 * 60 * 3  # Query Mihoyo for resin data every 3 hours
-    TEAPOT_CHECK_INTERVAL = 60 * 60 * 6  # Query Mihoyo for teapot data every 6 hours
+    CHECK_INTERVAL = 60 * 60 * 3  # Query Mihoyo for note data every 3 hours
 
     def __init__(self, bot: discord.Bot = None):
         self.bot = bot
@@ -30,140 +29,135 @@ class ResinCapReminder(commands.Cog):
             await asyncio.sleep(
                 random.randint(3 * 60, 5 * 60)
             )  # sleep randomly so everything doesn't start up at once.
-            self.resin_job.start()
-            self.teapot_job.start()
+            self.periodic_check.start()
             self.start_up = True
 
     @tasks.loop(seconds=CHECK_INTERVAL, reconnect=False)
-    async def resin_job(self):
+    async def periodic_check(self):
+        tasks = []
         for discord_id in session.execute(
             select(GenshinUser.discord_id.distinct())
         ).scalars():
-            try:
-                await self.check_resin(discord_id)
-            except Exception:
-                logging.exception(f"Cannot check resin for {discord_id}")
+            tasks.append(asyncio.create_task(self.check_accounts(discord_id)))
+        await asyncio.gather(*tasks)
 
-    @tasks.loop(seconds=TEAPOT_CHECK_INTERVAL, reconnect=False)
-    async def teapot_job(self):
-        for discord_id in session.execute(
-            select(GenshinUser.discord_id.distinct())
-        ).scalars():
-            try:
-                await self.check_teapot(discord_id)
-            except Exception:
-                logging.exception(f"Cannot check teapot for {discord_id}")
-
-    async def check_resin(
-        self, discord_id: int, max_time_awaited: float = CHECK_INTERVAL
+    async def check_accounts(
+        self, discord_id: int,
     ):
-        min_remaining_time = 60 * 60 * 24 * 7
-
         for account in session.execute(
             select(GenshinUser).where(GenshinUser.discord_id == discord_id)
         ).scalars():
-            if not account.settings[Preferences.RESIN_REMINDER]:
-                return
+            teapot_reminder = account.settings[Preferences.TEAPOT_REMINDER]
+            resin_reminder = account.settings[Preferences.RESIN_REMINDER]
+
+            if not teapot_reminder and not resin_reminder:
+                continue
 
             gs: genshin.GenshinClient = account.client
-            capped_uids = []
 
             for uid in account.genshin_uids:
                 notes = await gs.get_notes(uid)
-                reminder = session.get(ScheduledItem, (uid, self.RESIN_KEY))
 
-                if notes.current_resin == notes.max_resin:
-                    if not reminder:
-                        capped_uids.append(str(uid))
-                else:
-                    if reminder:
-                        session.delete(reminder)
-                        session.commit()
-                    min_remaining_time = min(min_remaining_time, notes.until_resin_recovery)
+                if resin_reminder and notes.max_resin > 0:
+                    reminder = session.get(ScheduledItem, (uid, self.RESIN_KEY))
 
-            if capped_uids:
-                discord_user = await self.bot.fetch_user(discord_id)
-                channel = await discord_user.create_dm()
-                embed = discord.Embed(
-                    title="Your resin is capped!",
-                    description=f"UID: {', '.join(capped_uids)}",
-                    color=0xFF1100,
-                )
-                embed.set_footer(text="You can turn this notification off via /user settings")
-                await channel.send(embed=embed)
-                for uid in capped_uids:
-                    session.merge(
-                        ScheduledItem(
-                            id=uid,
-                            type=self.RESIN_KEY,
-                            scheduled_at=datetime.utcnow(),
-                            done=True,
-                        )
-                    )
-                session.commit()
+                    if notes.until_resin_recovery < self.CHECK_INTERVAL:
+                        if not reminder:
+                            loop = asyncio.get_event_loop()
+                            loop.create_task(self.notify_resin(account, uid, notes.until_resin_recovery))
+                    else:
+                        if reminder:
+                            session.delete(reminder)
+                            session.commit()
+
+                if teapot_reminder and notes.max_realm_currency > 0:
+                    reminder = session.get(ScheduledItem, (uid, self.TEAPOT_KEY))
+
+                    if notes.until_realm_currency_recovery < self.CHECK_INTERVAL:
+                        if not reminder:
+                            loop = asyncio.get_event_loop()
+                            loop.create_task(self.notify_teapot(account, uid, notes.until_realm_currency_recovery))
+                    else:
+                        if reminder:
+                            session.delete(reminder)
+                            session.commit()
 
             await gs.close()
 
-        await_time = min_remaining_time
-        if 0 < await_time < max_time_awaited:
-            logging.info(f"Checking again for {discord_id} in {await_time} seconds")
-            await asyncio.sleep(await_time)
-            await self.check_resin(discord_id, max_time_awaited - await_time)
-
-    async def check_teapot(
-        self, discord_id: int, max_time_awaited: float = TEAPOT_CHECK_INTERVAL
+    async def notify_resin(
+        self, account: GenshinUser, uid: int, in_seconds: float
     ):
-        min_remaining_time = 60 * 60 * 24 * 7
+        logging.info(f"Notifying {uid} for resin in {in_seconds:.3f} seconds.")
+        await asyncio.sleep(in_seconds)
 
-        for account in session.execute(
-            select(GenshinUser).where(GenshinUser.discord_id == discord_id)
-        ).scalars():
-            if not account.settings[Preferences.TEAPOT_REMINDER]:
-                return
-
-            gs: genshin.GenshinClient = account.client
-            capped_uids = []
-
-            for uid in account.genshin_uids:
-                notes = await gs.get_notes(uid)
-                reminder = session.get(ScheduledItem, (uid, self.TEAPOT_KEY))
-
-                if notes.current_realm_currency == notes.max_realm_currency > 0:
-                    if not reminder:
-                        capped_uids.append(str(uid))
-                else:
-                    if reminder:
-                        session.delete(reminder)
-                        session.commit()
-                    min_remaining_time = min(min_remaining_time, notes.until_realm_currency_recovery)
-
-            if capped_uids:
-                discord_user = await self.bot.fetch_user(discord_id)
-                channel = await discord_user.create_dm()
-                embed = discord.Embed(
-                    title="Your teapot currency is capped!",
-                    description=f"UID: {', '.join(capped_uids)}",
-                    color=0xFF1100,
-                )
-                embed.set_thumbnail(
-                    url="https://static.wikia.nocookie.net/gensin-impact/images/5/5a/Item_Serenitea_Pot.png")
-                embed.set_footer(text="You can turn this notification off via /user settings")
-                await channel.send(embed=embed)
-                for uid in capped_uids:
-                    session.merge(
-                        ScheduledItem(
-                            id=uid,
-                            type=self.TEAPOT_KEY,
-                            scheduled_at=datetime.utcnow(),
-                            done=True,
-                        )
-                    )
-                session.commit()
-
+        for i in range(5):
+            gs = account.client
+            notes = await gs.get_notes(uid)
             await gs.close()
 
-        await_time = min_remaining_time
-        if 0 < await_time < max_time_awaited:
-            logging.info(f"Checking again for {discord_id} in {await_time} seconds")
-            await asyncio.sleep(await_time)
-            await self.check_teapot(discord_id, max_time_awaited - await_time)
+            if notes.current_resin < notes.max_resin:
+                return
+
+            if notes.max_resin > 0:
+                break
+        else:
+            return
+
+        discord_user = await self.bot.fetch_user(account.discord_id)
+        channel = await discord_user.create_dm()
+        embed = discord.Embed(
+            title="Your resin is capped!",
+            description=f"UID: {uid}",
+            color=0xFF1100,
+        )
+        embed.set_footer(text="You can turn this notification off via /user settings")
+        await channel.send(embed=embed)
+        session.merge(
+            ScheduledItem(
+                id=uid,
+                type=self.RESIN_KEY,
+                scheduled_at=datetime.utcnow(),
+                done=True,
+            )
+        )
+        session.commit()
+
+    async def notify_teapot(
+        self, account: GenshinUser, uid: int, in_seconds: float
+    ):
+        logging.info(f"Notifying {uid} for teapot in {in_seconds:.3f} seconds.")
+        await asyncio.sleep(in_seconds)
+
+        for i in range(5):
+            gs = account.client
+            notes = await gs.get_notes(uid)
+            await gs.close()
+
+            if notes.current_realm_currency < notes.max_realm_currency:
+                return
+
+            if notes.max_realm_currency > 0:
+                break
+        else:
+            return
+
+        discord_user = await self.bot.fetch_user(account.discord_id)
+        channel = await discord_user.create_dm()
+        embed = discord.Embed(
+            title="Your teapot currency is capped!",
+            description=f"UID: {uid}",
+            color=0xFF1100,
+        )
+        embed.set_thumbnail(
+            url="https://static.wikia.nocookie.net/gensin-impact/images/5/5a/Item_Serenitea_Pot.png")
+        embed.set_footer(text="You can turn this notification off via /user settings")
+        await channel.send(embed=embed)
+        session.merge(
+            ScheduledItem(
+                id=uid,
+                type=self.TEAPOT_KEY,
+                scheduled_at=datetime.utcnow(),
+                done=True,
+            )
+        )
+        session.commit()
