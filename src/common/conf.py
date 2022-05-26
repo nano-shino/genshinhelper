d6c5a4782a04eae33f9bcfd08616cb6bd44a1a81
Nano <nanoshino@>
2022-05-25 23:18:29 -0700

    Update env file loading mechanism

--- src/common/conf.py
+++ src/common/conf.py
@@ -1,7 +1,15 @@
 import os
 
-DISCORD_BOT_TOKEN = os.getenv("BOT_TOKEN", "")
-IMAGE_HOSTING_CHANNEL_ID = int(os.getenv("IMAGE_HOSTING_CHANNEL_ID", "0"))
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DISCORD_BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+if not DISCORD_BOT_TOKEN:
+    raise RuntimeError("Bot token is not present")
+
+IMAGE_HOSTING_CHANNEL_ID = int(os.getenv("IMAGE_HOSTING_CHANNEL_ID") or "0")
 ROUTE_CHANNEL_IDS = list(
     map(int, filter(None, os.getenv("ROUTE_CHANNEL_IDS", "").split(",")))
 )
